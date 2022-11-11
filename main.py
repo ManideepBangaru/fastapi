a6654e6211410b853fcdeb52cdb9ec7c834de5a7
manideepbangaru <bmd994@gmail.com>
2022-11-11 17:52:59 +0000

    commit for the day

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 async def say_hello():
-    return {"Hello":"World !!!"}
+    return "Hello World !!!"
+
+if __name__ == "__main__":
+    uvicorn.run(app)
