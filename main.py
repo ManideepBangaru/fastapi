95cfcf371ae7cf9400673f7aa1f7e97c43a31eb3
manideepbangaru <bmd994@gmail.com>
2022-11-24 18:20:50 +0000

    added path and parameter

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ import uvicorn
 
 app = FastAPI()
 
-@app.get("/fastapi_app")
+@app.get("/fastapi_app/sayhello")
 async def say_hello():
     return "Hello World !!!"
+
+@app.get("/fastapi_app/sayhello/{name}")
+async def say_hello_name(name:str):
+    return "Hello %s !!!"%name
 
 if __name__ == "__main__":
     uvicorn.run(app)
