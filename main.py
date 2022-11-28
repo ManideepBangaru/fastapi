90a5d1a5a759ebe14534d2d822ff641933e95423
Manideep Bangaru <manideep@gameopedia.com>
2022-11-29 00:10:58 +0530

    updated query paramters

--- main.py
+++ main.py
@@ -1,32 +1,13 @@
-from enum import Enum
 from fastapi import FastAPI
 import uvicorn
 
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 app = FastAPI()
 
-@app.get("/fastapi_app/sayhello")
-async def say_hello():
-    return "Hello World !!!"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/fastapi_app/sayhello/{name}")
-async def say_hello_name(name:str):
-    return "Hello %s !!!"%name
-
-@app.get("/fastapi_app/models/{model_name}")
-async def get_model(model_name : ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name" : model_name}
-    
-    if model_name.value == "lenet":
-        return {"model name" : model_name}
-    
-    return "model name %s"%model_name
+@app.get("/items/")
+async def read_item(skip : int = 0, limit : int = 10):
+    return fake_items_db[skip : skip + limit]
 
 if __name__ == "__main__":
     uvicorn.run(app)
