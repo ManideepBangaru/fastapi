d30661f0799f6932ff2c30cf0f8a4b23d07cab2a
ManideepBangaru <bmd994@gmail.com>
2022-11-30 23:15:22 +0530

    updated the code

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
-import uvicorn
 from pydantic import BaseModel
+import uvicorn
 
 class Item(BaseModel):
     name : str
@@ -10,13 +10,24 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-@app.post("/items/")
-async def create_item(item : Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price with tax" : price_with_tax})
-    return item_dict
+# @app.post("/items/")
+# async def create_item(item : Item):
+#     item_dict = item.dict()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price with tax" : price_with_tax})
+#     return item_dict
+
+# @app.put("/items/{item_id}")
+# async def create_item(item_id : int, item : Item):
+#     return {"item_id" : item_id, **item.dict()}
+
+@app.put("/items/{item_id}")
+async def create_item(item_id : int, item : Item, q : str | None = None):
+    result = {"item_id" : item_id, **item.dict()}
+    if q:
+        result.update({"q" : q})
+    return result
 
 if __name__ == "__main__":
     uvicorn.run(app)
