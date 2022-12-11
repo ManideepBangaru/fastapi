b53dc6b47944db6a53c466ffc0dc9135679b43bf
ManideepBangaru <bmd994@gmail.com>
2022-12-11 11:24:53 +0530

    added fastapi_app to the route

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = FastAPI()
 
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-@app.get("/items/{item_id}")
+@app.get("/fastapi_app/items/{item_id}")
 async def read_item(item_id : str, q : str | None = None, short : bool = False):
     item = {"item" : item_id}
     if q:
@@ -14,7 +14,7 @@ async def read_item(item_id : str, q : str | None = None, short : bool = False):
         item.update({"description" : "This is an amazing item that has a long description"})
     return item
 
-@app.get("/users/{user_id}/items/{item_id}")
+@app.get("/fastapi_app/users/{user_id}/items/{item_id}")
 async def read_user_item(user_id : int, item_id : str, q : str | None = None, short : bool = False):
     item = {"item_id" : item_id, "owner_id" : user_id}
     if q:
@@ -23,7 +23,7 @@ async def read_user_item(user_id : int, item_id : str, q : str | None = None, sh
         item.update({"description" : "This is an amazing item that has a long description"})
     return item
 
-@app.get("/names/{name_id}")
+@app.get("/fastapi_app/names/{name_id}")
 async def read_user_name(name_id : int, needy : str):
     users = {"user_id" : name_id, "user_name" : needy}
     return users
