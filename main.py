a834d638bdadb673a32548f4b6f8d561d3d97c07
ManideepBangaru <bmd994@gmail.com>
2022-12-18 18:05:52 +0530

    completed query paramteters and string validations

--- main.py
+++ main.py
@@ -1,32 +1,50 @@
-from enum import Enum
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 import uvicorn
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
 
 app = FastAPI()
 
-@app.get("/fastapi_app/sayhello")
-async def say_hello():
-    return "Hello World !!!"
+@app.get("/items/")
+async def read_items(q : str = Query(default=... , min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
 
-@app.get("/fastapi_app/sayhello/{name}")
-async def say_hello_name(name:str):
-    return "Hello %s !!!"%name
+# @app.get("/items/")
+# async def read_items(q : str | None = Query(default = "fixedquery", min_length=3, max_length = 50)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
 
-@app.get("/fastapi_app/models/{model_name}")
-async def get_model(model_name : ModelName):
-    if model_name is ModelName.alexnet:
-        return {"model_name" : model_name}
-    
-    if model_name.value == "lenet":
-        return "model name is len %s"%model_name
-    
-    return "model name %s"%model_name
+# @app.get("/items/")
+# async def read_items(q : str | None = Query(default = None, min_length=3, max_length = 50, regex="[A-Z][a-z][0-5]")):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# @app.get("/items/")
+# async def read_items(q : str | None = Query(default = None, min_length=3, max_length = 50)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.get("/items/")
+# async def read_items(q : str | None = Query(default = None, max_length = 50)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+# @app.get("/items/")
+# async def read_items(q:str | None=None):
+#     results = {"items" : [{"item_id": "Foo"},{"item_id" : "bar"}]}
+#     if q:
+#         results.update({"q" : q})
+#     return results
 
 if __name__ == "__main__":
     uvicorn.run(app)
