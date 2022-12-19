0648fed2963cabc1c427e4627a96a764a440e2ee
Manideep Bangaru <manideep@gameopedia.com>
2022-12-19 17:23:52 +0530

    updated body fields

--- main.py
+++ main.py
@@ -1,42 +1,19 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 import uvicorn
 
 app = FastAPI()
 
 class Item(BaseModel):
     name : str
-    description : str | None = None
-    price : float
+    description : str | None = Field(default=None, title="The description of the item", max_length=300)
+    price : float = Field(gt = 0, description= "The price must be greater than zero")
     tax : float | None = None
 
-class User(BaseModel):
-    username : str
-    fullname : str | None = None
-
-# Multiple body parameters
 @app.put("/items/{item_id}")
-async def update_items(item_id : int, item1 : Item, user : User, importance : int = Body()):
-    results = {"item_id" : item_id, "item" : item1, "user" : user, "importance" : importance}
+async def update_item(item_id : int, item: Item = Body(embed=True)):
+    results = {"item_id":item_id, "item" : item}
     return results
-
-# # Multiple body parameters
-# @app.put("/items/{item_id}")
-# async def update_items(item_id : int, item1 : Item, user : User):
-#     results = {"item_id" : item_id, "item" : item1, "user" : user}
-#     return results
-
-# @app.put("/items/{item_id}")
-# async def update_item(*,\
-#     item_id:int = Path(title="The id of the item to get", ge=0, le=1000),
-#     q : str | None = None,
-#     item : Item | None = None):
-#     results = {"item_id" : item_id}
-#     if q:
-#         results.update({"q" : q})
-#     if item:
-#         results.update({"item" : item})
-#     return results
 
 if __name__ == "__main__":
     uvicorn.run(app)
