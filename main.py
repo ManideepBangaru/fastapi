022ccd974314d44a6a9bf02d0f17b85f3c28b86e
Manideep Bangaru <manideep@gameopedia.com>
2022-12-19 18:23:36 +0530

    added body nested models

--- main.py
+++ main.py
@@ -1,19 +1,72 @@
-from fastapi import FastAPI, Body
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 import uvicorn
 
 app = FastAPI()
 
+# image url
+class Image(BaseModel):
+    url : HttpUrl
+    name : str
+
 class Item(BaseModel):
     name : str
-    description : str | None = Field(default=None, title="The description of the item", max_length=300)
-    price : float = Field(gt = 0, description= "The price must be greater than zero")
+    description : str | None
+    price : float
     tax : float | None = None
+    tags : set[str] = set()
+    images : list[Image] | None = None
 
 @app.put("/items/{item_id}")
-async def update_item(item_id : int, item: Item = Body(embed=True)):
-    results = {"item_id":item_id, "item" : item}
+async def read_item(item_id : int, item : Item):
+    results = {"item_id" : item_id, "item" : item}
     return results
+
+
+# General way - image url
+# class Image(BaseModel):
+#     url : str
+#     name : str
+
+# class Item(BaseModel):
+#     name : str
+#     description : str | None
+#     price : float
+#     tax : float | None = None
+#     tags : set[str] = set()
+#     image : Image | None = None
+
+# @app.put("/items/{item_id}")
+# async def read_item(item_id : int, item : Item):
+#     result = {"item_id" : item_id, "item" : item}
+#     return result
+
+
+# Python has a specific way to declare lists with internal types or "type parameters"
+# class Item(BaseModel):
+#     name : str
+#     description : Union[str, None] = None
+#     price : float
+#     tax : float
+#     tags : List[str] = []
+
+# @app.put("/items/{item_id}")
+# async def read_item(item_id : int, item : Item):
+#     results = {"item id" : item_id, "item" : item}
+#     return results
+
+# General way
+# class Item(BaseModel):
+#     name : str
+#     description : str
+#     price : float
+#     tax : float
+#     tags : list = []
+
+# @app.put("/items/{item_id}")
+# async def update_item(item_id : int, item: Item):
+#     results = {"item_id":item_id, "item" : item}
+#     return results
 
 if __name__ == "__main__":
     uvicorn.run(app)
