6eba6883150c2d2ec09fa96a1455aaa81cc3e4cb
Manideep Bangaru <manideep@gameopedia.com>
2022-12-19 19:42:31 +0530

    added form data

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
+from fastapi import FastAPI, Form
 import uvicorn
 
 app = FastAPI()
 
-class UserIn(BaseModel):
-    username : str
-    password : str
-    email : EmailStr
-    full_name : str | None = None
-
-class UserOut(BaseModel):
-    username : str
-    email : EmailStr
-    full_name  : str | None = None
-
-class Item(BaseModel):
-    Name : str
-    Description : str | None = None
-    Price : float
-    tax : float | None = None
-    tags : list[str] = []
-
-@app.post("/items/", response_model=Item)
-async def create_item(item : Item):
-    return item
-
-@app.post("/user/", response_model=UserOut)
-async def create_user(user : UserIn):
-    return user
+@app.post("/login/")
+async def login(username : str = Form(), password : str = Form()):
+    return {"username" : username}
 
 if __name__ == "__main__":
     uvicorn.run(app)
