76b088993bbe3ec49442e55e2faf8615e9866c71
Manideep Bangaru <manideep@gameopedia.com>
2022-12-19 17:03:40 +0530

    updated

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 from pydantic import BaseModel
 import uvicorn
 
-class Item(BaseModel):
-    name : str
-    description : str | None
-    price : float
-    tax : float | None
-
 app = FastAPI()
 
-# @app.post("/items/")
-# async def create_item(item : Item):
-#     item_dict = item.dict()
-#     if item.tax:
-#         price_with_tax = item.price + item.tax
-#         item_dict.update({"price with tax" : price_with_tax})
-#     return item_dict
+class Item(BaseModel):
+    name : str
+    description : str | None = None
+    price : float
+    tax : float | None = None
+
+class User(BaseModel):
+    username : str
+    fullname : str | None = None
+
+# Multiple body parameters
+@app.put("/items/{item_id}")
+async def update_items(item_id : int, item1 : Item, user : User, importance : int = Body()):
+    results = {"item_id" : item_id, "item" : item1, "user" : user, "importance" : importance}
+    return results
+
+# # Multiple body parameters
+# @app.put("/items/{item_id}")
+# async def update_items(item_id : int, item1 : Item, user : User):
+#     results = {"item_id" : item_id, "item" : item1, "user" : user}
+#     return results
 
 # @app.put("/items/{item_id}")
-# async def create_item(item_id : int, item : Item):
-#     return {"item_id" : item_id, **item.dict()}
-
-@app.put("/items/{item_id}")
-async def create_item(item_id : int, item : Item, q : str | None = None):
-    result = {"item_id" : item_id, **item.dict()}
-    if q:
-        result.update({"q" : q})
-    return result
+# async def update_item(*,\
+#     item_id:int = Path(title="The id of the item to get", ge=0, le=1000),
+#     q : str | None = None,
+#     item : Item | None = None):
+#     results = {"item_id" : item_id}
+#     if q:
+#         results.update({"q" : q})
+#     if item:
+#         results.update({"item" : item})
+#     return results
 
 if __name__ == "__main__":
     uvicorn.run(app)
