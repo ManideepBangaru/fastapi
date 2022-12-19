19ee1b7a75ba4f692b0597fad018b2f1f2b48152
ManideepBangaru <bmd994@gmail.com>
2022-12-19 22:48:09 +0530

    updated dependencies yield

--- main.py
+++ main.py
@@ -1,22 +1,40 @@
-from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from pydantic import BaseModel
-from datetime import datetime
-import uvicorn
+from fastapi import Depends
 
-app = FastAPI()
+class MySuperContextManager:
+    def __init__(self):
+        self.db = DBSession()
 
-fake_db = {}
+    def __enter__(self):
+        return self.db
 
-class Item(BaseModel):
-    title : str
-    timestamp : datetime
-    description : str | None = None
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.db.close()
 
-@app.put("/items/{id}")
-def update_item(id : str, item : Item):
-    json_compatible_item_data = jsonable_encoder(item)
-    fake_db[id] = json_compatible_item_data
 
-if __name__ == "__main__":
-    uvicorn.run(app)
+async def get_db():
+    with MySuperContextManager() as db:
+        yield db
+
+
+# async def dependency_a():
+#     dep_a = generate_dep_a()
+#     try:
+#         yield dep_a
+#     finally:
+#         dep_a.close()
+
+
+# async def dependency_b(dep_a=Depends(dependency_a)):
+#     dep_b = generate_dep_b()
+#     try:
+#         yield dep_b
+#     finally:
+#         dep_b.close(dep_a)
+
+
+# async def dependency_c(dep_b=Depends(dependency_b)):
+#     dep_c = generate_dep_c()
+#     try:
+#         yield dep_c
+#     finally:
+#         dep_c.close(dep_b)
