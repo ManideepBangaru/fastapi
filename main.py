8a4c61668a3be9ddb00944d4c657e575a80a92e3
Manideep Bangaru <manideep@gameopedia.com>
2022-12-26 20:30:04 +0530

    updated feature security

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 app = FastAPI()
 
+list_of_usernames = []
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@app.get("/items/")
-async def read_items(token : str = Depends(oauth2_scheme)):
-    return {"token" : token}
+@app.post("/token")
+async def token_generate(form_data : OAuth2PasswordRequestForm = Depends()):
+    print(form_data)
+    return {"access_token":form_data.username, "token_type":"bearer"}
+
+@app.post("/users/profilepic")
+async def profile_pic(token : str = Depends(oauth2_scheme)):
+    print(token)
+    return {"User" : "Manideep","profile_pic" : "My_face"}
