7c31dfdca16d9b8abd20d80ad633c51e681cc32e
Manideep Bangaru <manideep@gameopedia.com>
2022-12-26 20:02:37 +0530

    updated upload files way through fastapi

--- main.py
+++ main.py
@@ -1,42 +1,21 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, Request, Form, File, UploadFile
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="html_templates/")
 
-# Multiple uploads
-@app.post("/files/")
-async def create_files(files : list[bytes] = File()):
-    return {"file_sizes" : [len(file) for file in files]}
+@app.get("/", response_class=HTMLResponse)
+def file_up_req_page(request : Request):
+    return templates.TemplateResponse("homepage.html", context={"request" : request})
 
-@app.post("/uploadfiles/")
-async def create_upload_files(files : list[UploadFile]):
-    return {"filenames": [file.filename for file in files]}
-
-@app.get("/")
-async def main():
-    content = """
-                <body>
-                <form action="/files/" enctype="multipart/form-data" method="post">
-                    <input name="files" type="file" multiple>
-                    <input type="submit">
-                </form>
-                <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
-                    <input name="files" type="file" multiple>
-                    <input type="submit">
-                </form>
-                </body>
-            """
-    return HTMLResponse(content=content)
-
-# upload a file
-# @app.post("/files/")
-# async def create_file(file : bytes = File()):
-#     return {"file_size" : len(file)}
-
-# @app.post("/uploadfile/")
-# async def create_upload_file(file : UploadFile):
-#     return {"filename" : file.filename}
+@app.post("/file_status_page", response_class=HTMLResponse)
+async def file_up_status(request = Request, firstname : str = Form(...), assign_file : UploadFile = File(...)):
+    firstname = firstname
+    assign_file_content = await assign_file.read()
+    print(assign_file_content)
+    return templates.TemplateResponse("file_status.html", context={"request" : request, "firstname" : firstname})
 
 if __name__ == "__main__":
     uvicorn.run(app)
