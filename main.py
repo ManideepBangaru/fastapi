8a5e73737419516121d1c64f8a3ecf8ca799ba0d
Manideep Bangaru <manideep@gameopedia.com>
2022-12-26 20:48:19 +0530

    switching to macbook

--- main.py
+++ main.py
@@ -1,33 +1,5 @@
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from fastapi.background import BackgroundTasks
-import time
+from fastapi import FastAPI, Body, Request
 
 app = FastAPI()
 
-list_of_usernames = []
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def handle_email_background(email : str, data : str):
-    print(email)
-    print(data)
-    for i in range(100):
-        print(i)
-        time.sleep(0.1)
-
-@app.get("/users/email")
-async def handle_email(email : str , background_task : BackgroundTasks):
-    print(email)
-    background_task.add_task(handle_email_background, email, "This is a sample background task manager")
-    return {"user" : "Manideep", "message" : "Mail Sent"}
-
-@app.post("/token")
-async def token_generate(form_data : OAuth2PasswordRequestForm = Depends()):
-    print(form_data)
-    return {"access_token":form_data.username, "token_type":"bearer"}
-
-@app.post("/users/profilepic")
-async def profile_pic(token : str = Depends(oauth2_scheme)):
-    print(token)
-    return {"User" : "Manideep","profile_pic" : "My_face"}
+@app.get("/")
