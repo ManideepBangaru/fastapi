7546f53582b113e9680692d7821d822e17c527da
Manideep Bangaru <manideep@gameopedia.com>
2022-12-26 20:44:32 +0530

    completed this module

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import Depends, FastAPI
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi.background import BackgroundTasks
+import time
 
 app = FastAPI()
 
 list_of_usernames = []
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def handle_email_background(email : str, data : str):
+    print(email)
+    print(data)
+    for i in range(100):
+        print(i)
+        time.sleep(0.1)
+
+@app.get("/users/email")
+async def handle_email(email : str , background_task : BackgroundTasks):
+    print(email)
+    background_task.add_task(handle_email_background, email, "This is a sample background task manager")
+    return {"user" : "Manideep", "message" : "Mail Sent"}
 
 @app.post("/token")
 async def token_generate(form_data : OAuth2PasswordRequestForm = Depends()):
